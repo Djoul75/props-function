b0a1681701fc98b09eb15d116245c1e46aa4df67
Jules Gorce <jules.gorce@gmail.com>
2022-09-01 15:12:07 +0200

    first commit

---
+++ src/components/maman.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Maman extends Component {
+  state = {
+    messageMaman: null,
+    messageToto: null
+  }
+
+  render() {
+    return (
+      <div>
+        test
+      </div>
+    )
+  }
+}
+
+export default Maman
