c78c363a2198c846617f2bdc70fe213266907c57
Jules Gorce <jules.gorce@gmail.com>
2022-09-01 15:52:25 +0200

    exercice sur le passage de fonction dans les props

--- src/components/maman.js
+++ src/components/maman.js
@@ -4,31 +4,24 @@ import Toto from './toto'
 class Maman extends Component {
   state = {
     messageMaman: null,
-    messageToto: null
+    messageToto: null,
+    disabled: true
   }
 
-  ordreMaman = () => {
-    this.setState({
-      messageMaman: 'Vas ranger ta chambre'
-    })
-  }
+  ordreMaman = msg => this.setState({messageMaman: msg, disabled: false})
 
-  reponseToto = () => {
-    this.setState({
-      messageToto: "D'accord maman"
-    })
-  }
+  reponseToto = msg => this.setState({messageToto: msg})
 
   render() {
     return (
       <div>
         <h1>Maman</h1>
-        <button onClick={this.ordreMaman}>Ordre de la mère</button>
+        <button onClick={() => this.ordreMaman("Vas ranger ta chambre")}>Ordre de la mère</button>
         <p>{this.state.messageMaman}</p>
 
         <hr />
 
-        <Toto name="Toto" leState={this.state} reponseToto={this.reponseToto}/>
+        <Toto name="Toto" leState={this.state} reponseTotoProps={this.reponseToto}/>
       </div>
     )
   }
--- src/components/toto.js
+++ src/components/toto.js
@@ -1,15 +1,13 @@
 import React from 'react'
 
 const Toto = (props) => {
-
-  const btnResponseToto = props.leState.messageMaman !== null
-    ? (<button onClick={props.reponseToto}>Réponse</button>)
-    : (<button disabled>Réponse</button>)
-
   return(
     <div>
         <h1>{props.name}</h1>
-        {btnResponseToto}
+        <button
+          disabled={props.leState.disabled}
+          onClick={() => props.reponseTotoProps("D'accord")}
+        >Réponse</button>
         <p>{props.leState.messageToto}</p>
     </div>
   )
